0582f64d66f7b5752dbba69af290bf32ee54f159
Evan Hicks <evahicks@cs.du.edu>
2017-06-29 20:11:45 -0600

    Completed COMP-3705 lab 4

---
+++ labs/lab4/lab4.js
@@ -0,0 +1,62 @@
+/**
+ * Created by Evan on 6/29/17.
+ */
+class lab4 {
+
+  syncFileRead(filename) {
+    var fs = require('fs');
+
+    var data = fs.readFileSync(filename);
+
+    return data.toString();
+  }
+
+  asyncFileRead(filename, callback) {
+    var fs = require('fs');
+
+    fs.open(filename, 'r+', function(data) {
+      return callback(data);
+    });
+  }
+
+  compressFileStream(fileIn, fileOut) {
+    var fs = require('fs');
+    var zlip = require('zlib');
+
+     // Compress the file input.txt to input.txt.gz
+    var stream = fs.createReadStream(fileIn)
+        .pipe(zlip.createGzip())
+        .pipe(fs.createWriteStream(fileOut));
+
+    console.log('File Compressed.');
+    return stream;
+  }
+
+  decompressFileStream(fileIn, fileOut) {
+    var fs = require('fs');
+    var zlip = require('zlib');
+
+     // Compress the file input.txt to input.txt.gz
+    var stream = fs.createReadStream(fileIn)
+        .pipe(zlip.createGunzip())
+        .pipe(fs.createWriteStream(fileOut));
+
+    console.log('File Decompressed.');
+    return stream;
+  }
+
+  listDirectoryContents(directory, callback) {
+    var fs = require('fs');
+
+    //var arr = [];
+    fs.readdir(directory, function(err,files) {
+      if(err) {
+        console.log(err);
+      }
+      return callback(files);
+    });
+  }
+
+}
+
+export {lab4};
