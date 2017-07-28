5afd3b206e2bc15dd235031b9d403e161c026cfc
Evan Hicks <evahicks@cs.du.edu>
2017-07-27 19:03:36 -0600

    Completed COMP-3705 lab 11

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -3,14 +3,48 @@ const ngRoute = require('angular-route');
 import routing from './main.routes';
 
 export class MainController {
-  /*@ngInject*/
-  constructor($http) {
+   /*@ngInject*/
+  constructor($http, User) {
     this.$http = $http;
+    this.User = User;
+    this.setData();
+    this.getUserData();
+  }
+
+  setData() {
     this.values = ['first', 'second', 'third'];
+    this.valueToSquare = 4;
+  }
+
+  getUserData() {
+    this.User.getAllUsers()
+         .then(response => {
+           this.users = response.data;
+         })
+         .catch(error => {
+           console.error(error);
+         });
   }
 
   $onInit() {
   }
+}
+
+export function UserService($http) {
+  'ngInject';
+  var User = {
+    getAllUsers() {
+      return $http.get('/api/users/');
+    }
+  };
+  return User;
+}
+
+export function SquareFilter() {
+  var squareFunction = function(value) {
+    return value * value;
+  };
+  return squareFunction;
 }
 
 export default angular.module('comp3705App.main', [ngRoute])
@@ -21,4 +55,6 @@ export default angular.module('comp3705App.main', [ngRoute])
     controllerAs: 'mainController'
 
   })
+   .service('User', UserService)
+   .filter('Square', SquareFilter)
   .name;
